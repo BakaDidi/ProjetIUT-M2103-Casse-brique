7861657cfadac6766722f0d52510a57498fb2eb9
Dylan Henocque <dylan.henocque@gmail.com>
2021-06-01 22:00:09 +0200

    Ajout d'un constructeur de raquette

--- ProjetCasseBriques/ProjetCasseBriques/Raquette.h
+++ ProjetCasseBriques/ProjetCasseBriques/Raquette.h
@@ -1,10 +1,19 @@
 class Raquette
 {
-public:
-	int Pos_R_X;
-	int Pos_R_Y;
-	int larg_R;
-	void update(int Pos_R_X, int Pos_R_Y);
-private:
-	int vitesse;
+	public:
+		int Pos_R_X;
+		int Pos_R_Y;
+		int larg_R;
+		void update(int Pos_R_X, int Pos_R_Y);
+
+	private:
+		int vitesse;
+
+	public Raquette(int largeur,int vitesseRaquette)
+	{
+		vitesse = vitesseRaquette;
+		larg_R = largeur;
+		Pos_R_X = 50;
+		Pos_R_Y = 30;
+	}
 };
