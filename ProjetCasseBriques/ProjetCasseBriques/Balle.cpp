e0554a48fe4df9789de34c60092431edfb8e7e84
Dylan Henocque <dylan.henocque@gmail.com>
2021-06-01 21:28:08 +0200

    Ajout des méthodes balle:rebondir_M et balle::rebondir_R

--- ProjetCasseBriques/ProjetCasseBriques/Balle.cpp
+++ ProjetCasseBriques/ProjetCasseBriques/Balle.cpp
@@ -1,7 +1,19 @@
 #include "Balle.h"
 
-void Balle::rebondir_R()
+void Balle::rebondir_R(int Pos_B_X, int Pos_B_Y, int Pos_R_X, int Pos_R_Y, ref int dir)
 {
+	if ((0 < (Pos_B_X - Pos_R_X) && ((Pos_B_X - Pos_R_X < larg_R))) && ((0 < (Pos_B_Y)) && (Pos_B_Y == Pos_BR_Y)))
+	{
+		if (dir == 3)
+		{
+			dir = 1;
+		}
+		else if (dir == 4)
+		{
+			dir = 2;
+		}
+	}
+
 }
 
 void Balle::rebondir_BR(int Pos_B_X, int Pos_B_Y, int Pos_BR_X, int Pos_BR_Y, ref int dir)
@@ -55,8 +67,44 @@ void Balle::rebondir_BR(int Pos_B_X, int Pos_B_Y, int Pos_BR_X, int Pos_BR_Y, re
 	}
 }
 
-void Balle::rebondir_M()
+void Balle::rebondir_M(int Pos_B_X, int Pos_B_Y, ref int dir)
 {
+	if (dir == 1)
+	{
+		if (Pos_B_X == Xmax)
+		{
+			dir = 2;
+		}
+		else if (Pos_B_Y == Ymax)
+		{
+			dir = 3;
+		}
+	}
+	else if (dir == 2)
+	{
+		if (Pos_B_X == Xmin)
+		{
+			dir = 1;
+		}
+		else if (Pos_B_Y == Ymax)
+		{
+			dir = 4;
+		}
+	}
+	else if (dir == 3)
+	{
+		if (Pos_B_X == Xmax)
+		{
+			dir = 4;
+		}
+	}
+	else if (dir == 4)
+	{
+		if (Pos_B_X == Xmin)
+		{
+			dir = 3
+		}
+	}
 }
 
 void Balle::update(ref int Pos_B_X, ref int Pos_B_Y, int dir)
@@ -82,5 +130,3 @@ void Balle::update(ref int Pos_B_X, ref int Pos_B_Y, int dir)
 		Pos_B_Y = Pos_B_Y - 1;
 	}
 }
-
-
--- ProjetCasseBriques/ProjetCasseBriques/Balle.h
+++ ProjetCasseBriques/ProjetCasseBriques/Balle.h
@@ -5,9 +5,9 @@ class Balle
 public:
 	int Pos_B_X;
 	int Pos_B_Y;
-	void rebondir_R();
-	void Balle::rebondir_BR(int Pos_B_X, int Pos_B_Y, int Pos_BR_X, int Pos_BR_Y, ref int dir);
-	void rebondir_M();
+	void rebondir_R(int Pos_B_X, int Pos_B_Y, int Pos_R_X, int Pos_R_Y, ref int dir);
+	void rebondir_BR(int Pos_B_X, int Pos_B_Y, int Pos_BR_X, int Pos_BR_Y, ref int dir);
+	void rebondir_M(int Pos_B_X, int Pos_B_Y, ref int dir);
 	void update(ref int Pos_B_X, ref int Pos_B_Y, int dir);
 private:
 	int Dir;
