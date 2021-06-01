41e67099868a27a02fef889590da8773d7a60490
Dylan Henocque <dylan.henocque@gmail.com>
2021-06-01 18:45:47 +0200

    Ajout de la fonction update dans balle.cpp

--- ProjetCasseBriques/ProjetCasseBriques/Balle.cpp
+++ ProjetCasseBriques/ProjetCasseBriques/Balle.cpp
@@ -14,7 +14,26 @@ void rebondir_M()
 
 }
 
-void update()
+void update(ref int Pos_B_X, ref int Pos_B_Y, int dir)
 {
-
+	if dir == 1
+	{
+		Pos_B_X = Pos_B_X + 1;
+		Pos_B_Y = Pos_B_Y + 1;
+ 	}
+	else if dir == 2 
+	{
+		Pos_B_X = Pos_B_X - 1;
+		Pos_B_Y = Pos_B_Y + 1;
+	}
+	else if dir == 3
+	{
+		Pos_B_X = Pos_B_X + 1;
+		Pos_B_Y = Pos_B_Y - 1;
+	}
+	else if dir == 4
+	{
+		Pos_B_X = Pos_B_X - 1;
+		Pos_B_Y = Pos_B_Y - 1;
+	}
 }
